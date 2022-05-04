f62ff32effd64cf7de75b635c1a2a3d0ff6b700d
Thinnawarth Mathuros <thinnawarth@gmail.com>
2022-05-04 14:53:24 +0700

    [Edited] try button component

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,21 @@
 "use strict";
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Button", {
+  enumerable: true,
+  get: function get() {
+    return _Button.default;
+  }
+});
+Object.defineProperty(exports, "Pagination", {
+  enumerable: true,
+  get: function get() {
+    return _Pagination.default;
+  }
+});
+
 var _Button = _interopRequireDefault(require("./components/Button/Button"));
 
 var _Pagination = _interopRequireDefault(require("./components/Pagination/Pagination"));
