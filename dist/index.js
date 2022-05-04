c23f605d994cdad24db3554771f55e33302d0a63
Thinnawarth Mathuros <thinnawarth@gmail.com>
2022-05-05 02:50:26 +0700

    [Edited] change project to typescript

--- dist/index.js
+++ dist/index.js
@@ -1,23 +1,2 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-Object.defineProperty(exports, "Button", {
-  enumerable: true,
-  get: function get() {
-    return _Button.default;
-  }
-});
-Object.defineProperty(exports, "Pagination", {
-  enumerable: true,
-  get: function get() {
-    return _Pagination.default;
-  }
-});
-
-var _Button = _interopRequireDefault(require("./components/Button/Button"));
-
-var _Pagination = _interopRequireDefault(require("./components/Pagination/Pagination"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+import Button from "./components/Button";
+export { Button };
--- src/lib/index.js
+++
@@ -1,4 +0,0 @@
-import Button from "./components/Button/Button";
-import Pagination from "./components/Pagination/Pagination";
-
-export {Button,Pagination}
